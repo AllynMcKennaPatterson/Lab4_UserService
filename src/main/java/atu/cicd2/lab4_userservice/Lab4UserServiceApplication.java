bdcac6db0b84f32641b7857b961eb3efb91683f2
G00392765@atu.ie <G00392765@atu.ie>
2023-10-11 10:04:01 +0100

    Enabled Feign Clients

--- src/main/java/atu/cicd2/lab4_userservice/Lab4UserServiceApplication.java
+++ src/main/java/atu/cicd2/lab4_userservice/Lab4UserServiceApplication.java
@@ -2,7 +2,9 @@ package atu.cicd2.lab4_userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class Lab4UserServiceApplication {
 
