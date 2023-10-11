00dbabb98960bdd7d6b40dac47b84c46c850feb1
G00392765@atu.ie <G00392765@atu.ie>
2023-10-11 09:54:24 +0100

    Initial commit

---
+++ src/main/java/atu/cicd2/lab4_userservice/Lab4UserServiceApplication.java
@@ -0,0 +1,13 @@
+package atu.cicd2.lab4_userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab4UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab4UserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/atu/cicd2/lab4_userservice/Lab4UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package atu.cicd2.lab4_userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab4UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
