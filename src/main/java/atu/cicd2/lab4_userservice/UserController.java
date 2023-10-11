953822abdcbaaa1eee4659ef51e05cd0d1780484
G00392765@atu.ie <G00392765@atu.ie>
2023-10-11 10:21:06 +0100

    Added PostMapping

--- src/main/java/atu/cicd2/lab4_userservice/UserController.java
+++ src/main/java/atu/cicd2/lab4_userservice/UserController.java
@@ -1,7 +1,12 @@
 package atu.cicd2.lab4_userservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class UserController {
@@ -11,5 +16,13 @@ public class UserController {
     @Autowired
     public UserController(RegistrationServiceClient registrationServiceClient){
         this.registrationServiceClient = registrationServiceClient;
+    }
+
+    @PostMapping("confirm-and-register")
+    public Map<String, String> confirmAndRegister(@RequestBody UserDetails userDetails){
+        String confirm =registrationServiceClient.someDetails(userDetails);
+        Map<String, String> responseMessage = new HashMap<>();
+        responseMessage.put("message", confirm);
+        return responseMessage;
     }
 }
