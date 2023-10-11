e8db1b4f54cd053d6ce2cbed224f64ed38a62c0f
G00392765@atu.ie <G00392765@atu.ie>
2023-10-11 10:07:11 +0100

    Created UserDetails class

---
+++ src/main/java/atu/cicd2/lab4_userservice/UserDetails.java
@@ -0,0 +1,13 @@
+package atu.cicd2.lab4_userservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDetails {
+    private String name;
+    private String email;
+}
